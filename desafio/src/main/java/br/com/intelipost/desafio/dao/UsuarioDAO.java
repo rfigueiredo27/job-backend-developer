b7fbf39b595dc39dc21420b81058a3fec5ff44a5
rfigueiredo27 <renan.figueiredo@globo.com>
2018-06-19 23:39:26 -0300

    Criação das Classes UsuarioDAO que faz o acesso aos dados e seu respectivo modelo (Usuario) e a criação da Classe Role que gerencia a permissão do usuário.

---
+++ desafio/src/main/java/br/com/intelipost/desafio/dao/UsuarioDAO.java
@@ -0,0 +1,32 @@
+package br.com.intelipost.desafio.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import br.com.intelipost.desafio.models.Usuario;
+
+
+@Repository
+public class UsuarioDAO implements UserDetailsService{
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public Usuario loadUserByUsername(String email) {
+		List<Usuario> usuarios = manager.createQuery("select u from Usuario u where email = :email", Usuario.class)
+				.setParameter("email", email)
+				.getResultList();
+		
+		if(usuarios.isEmpty()) {
+			throw new UsernameNotFoundException("Usuario " + email + " não foi encontrado");
+		}
+		
+		return usuarios.get(0);
+	}
+}
